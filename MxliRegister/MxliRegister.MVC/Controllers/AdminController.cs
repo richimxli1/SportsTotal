2e695521a84c548ef2d58ed21291354c9a104bc2
DEV2-SGM-IT <51895069+DEV2-SGM-IT@users.noreply.github.com>
2020-01-23 23:00:41 -0700

    Initial Changes

---
+++ MxliRegister/MxliRegister.MVC/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MxliRegister.MVC.Controllers
+{
+    public class AdminController : Controller
+    {
+        /*   private readonly IHttpContextAccessor _httpContextAccessor;
+           private ISession _session => _httpContextAccessor.HttpContext.Session;
+
+
+           public AdminController(IHttpContextAccessor httpContextAccessor)
+           {
+               _httpContextAccessor = httpContextAccessor;
+           }
+            */
+        public IActionResult Login()
+        {
+
+         /*   //model.UserName = "gustavo.balboa";
+            //model.RememberMe = true;
+            //Collaborator = collaboratorService.GetCollaborator(model.UserName);
+            //Collaborator.Photo = activeDirectoryService.GetCollaboratorPhoto(model.UserName);
+            System.Web.HttpContext.Current.Session[Constants.UserSession] = this.Collaborator;
+            System.Web.HttpContext.Current.Session[Constants.UserSessionRoles] = projectRoleService.GetUserRoles(this.Collaborator.Id);
+            FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+            */
+            
+            return View();
+        }
+
+        public IActionResult InsertTeam()
+        {
+            return View();
+        }
+        public IActionResult InsertPlayer()
+        {
+            return View();
+        }
+        public IActionResult SeePlayersRegister()
+        {
+            return View();
+        }
+    }
+}
